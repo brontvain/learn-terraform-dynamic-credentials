6ec59d1c42bbdaa79502fedca6fca48b9550e208
pbrontvain <pbrontvain@salesforce.com>
2024-05-30 13:33:14 -0400

    uno mas

--- azure/infra/main.tf
+++ azure/infra/main.tf
@@ -22,5 +22,5 @@ resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.3.0/24"]
+  address_prefixes     = ["10.0.4.0/24"]
 }
